ea7958f2300350e64301c167e51784da9affdcca
Edoardo Balducci <81587335+Bbalduzz@users.noreply.github.com>
2022-06-22 23:50:54 +0200

    Add files via upload

---
+++ content-script.js
@@ -0,0 +1,78 @@
+﻿function Func() {
+	return new Promise((resolve, reject) => {
+		var jsString = document.querySelector("#player >script:nth-child(2)")
+		if (!jsString) {
+			jsString = document.querySelector("#player >script:nth-child(1)")
+		}
+		if (jsString) {
+			jsString = jsString.innerHTML
+			jsString = `	var playerObjList = {};\n${jsString}`
+			var flashvars = jsString.match("flashvars_[0-9]{1,}")[0]
+			eval(jsString)
+			var jsObject = eval(flashvars)
+			resolve(jsObject)
+		}
+		jsString = document.querySelector("#video-player-bg > script:nth-child(6)")
+		if (jsString) {
+			jsString = jsString.innerHTML
+			console.log(jsString)
+			var videoType = []
+			var urlTitle = jsString.match(/setVideoTitle\('(.*?)'\);/)[1]
+			var urlLow = jsString.match(/setVideoUrlLow\('(.*?)'\);/)[1]
+			if (urlLow){
+				var obj ={
+					key: "Low",
+					val: urlLow,
+					video_title: urlTitle
+				}
+				videoType.push(obj)
+			}
+			var urlHigh = jsString.match(/setVideoUrlHigh\('(.*?)'\);/)[1]
+			if (urlHigh){
+				var obj ={
+					key: "High",
+					val: urlHigh,
+					video_title: urlTitle
+				}
+				videoType.push(obj)
+			}
+			browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+			if (request.cmd == 'test') 
+				sendResponse(videoType);
+	});
+		}
+	})
+}
+
+Func().then(res => {
+	var videoType = []
+	Object.keys(res['mediaDefinitions']).forEach((item) => {
+		var itemInfo = res['mediaDefinitions'][item]
+		if (itemInfo['format'] == 'mp4') {
+			var mp4UrlJson = getMp4Url(itemInfo['videoUrl']);
+			for (const mp4Url of mp4UrlJson){
+				var obj = {
+				key: mp4Url['quality'],
+				val: mp4Url['videoUrl'],
+				video_title:res.video_title
+				}
+			videoType.push(obj)
+			}
+		}
+	})
+	browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+		if (request.cmd == 'test') 
+			sendResponse(videoType);
+	});
+})
+
+function getMp4Url(url){
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", url , false);
+	xhr.onload = function(){
+	if (xhr.readyState === 4 && xhr.status === 200){
+		mp4UrlJson = JSON.parse(xhr.responseText);
+	}};
+	xhr.send();
+	return mp4UrlJson;
+}
---
+++ popup.js
@@ -0,0 +1,50 @@
+﻿function sendMessageToContentScript(message, callback) {
+    browser.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        browser.tabs.sendMessage(tabs[0].id, message, function (response) {
+            if (callback) callback(response);
+        });
+    });
+}
+sendMessageToContentScript({ cmd: 'test', value: 'test' }, function (videoType) {
+    if(videoType == null) return 
+    var boxEl = document.getElementsByTagName('ul')[0]
+    var videoStr = ''
+    videoType.forEach((item) => {
+        videoStr += `<li><label>Quality：<span> ${item.key} </span> </label> <button class="button down">Download</button> <button class="button copy">Get link</button></li>`
+    });
+    boxEl.innerHTML = videoStr
+    var dialog = document.getElementsByTagName("dialog")[0]
+    var dialog1 = document.getElementsByTagName("dialog")[1]
+    var downList = document.querySelectorAll(".down")
+    downList.forEach((item, index) => {
+        item.onclick = () => {
+            let reg =  /[\~\.\:\/\*\?\"\|\\\<\>]/g
+            browser.downloads.download({
+                url: videoType[index].val,
+                filename: videoType[index].video_title.replace(reg, '') + ".mp4"
+            })
+            dialog1.showModal();
+            setTimeout(() => {
+                dialog1.close();
+            }, 2000)
+        }
+    })
+
+    var copyList = document.querySelectorAll(".copy")
+    copyList.forEach((item, index) => {
+        item.onclick = () => {
+            var url = videoType[index].val
+            var oInput = document.createElement('input');
+            oInput.value = url;
+            document.body.appendChild(oInput);
+            oInput.select();
+            document.execCommand("Copy");
+            oInput.className = 'oInput';
+            oInput.style.display = 'none';
+            dialog.showModal();
+            setTimeout(() => {
+                dialog.close();
+            }, 1500)
+        }
+    })
+});
